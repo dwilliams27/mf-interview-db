11cccacb51ef3aad08283348194bc5b8b6d046cd
Farmerbilly27 <dmw2796@gmail.com>
2023-10-17 12:05:27 -0500

    add reporting tables

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Start DB by running:
 docker-compose up -d
 ```
 
-## Creating table
+## Creating tables
 
-Create the `pmt_files` table by:
+Create the tables by:
 * Opening PG admin in browser (http://localhost:5050/)
 * Logging in with email and password from docker-compose file
-* Execute contents of `setup.sql`
+* Executing contents of `setup.sql`
--- setup.sql
+++ setup.sql
@@ -2,5 +2,20 @@ CREATE TABLE pmt_files (
     file_uuid UUID PRIMARY KEY,
     file_name VARCHAR(255),
     file_status VARCHAR(16),
+    failed_payments INTEGER,
     upload_ts TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE src_funds (
+	id SERIAL PRIMARY KEY,
+	file_uuid UUID,
+	src_acct VARCHAR(255),
+	amt numeric
+);
+
+CREATE TABLE brnch_funds (
+	id SERIAL PRIMARY KEY,
+	file_uuid UUID,
+	src_brnch VARCHAR(255),
+	amt numeric
 );
